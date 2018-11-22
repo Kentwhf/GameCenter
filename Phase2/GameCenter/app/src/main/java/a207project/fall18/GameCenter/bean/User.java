66cf9c0b4868e6e576bc3307f7c7b07ee2cb8d03
Jackson <yibing.sheng@mail.utoronto.ca>
2018-11-21 21:33:07 -0500

    version 1

---
+++ Phase2/GameCenter/app/src/main/java/a207project/fall18/GameCenter/bean/User.java
@@ -0,0 +1,47 @@
+package a207project.fall18.GameCenter.bean;
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private String nickname;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    @Override
+    public String toString() {
+        return this.id+"/"+this.username+"/"+this.nickname+"/"+this.password;
+    }
+}
+
+
