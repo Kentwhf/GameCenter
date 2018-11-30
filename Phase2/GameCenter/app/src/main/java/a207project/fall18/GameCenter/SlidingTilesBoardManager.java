e5bda3ded964d33da547d9a380be23b822ee4f37
Shi Hao Wang <tigershihao.wang@mail.utoronto.ca>
2018-11-30 08:12:15 -0500

    Stash before pull.

--- Phase2/GameCenter/app/src/main/java/a207project/fall18/GameCenter/SlidingTilesBoardManager.java
+++ Phase2/GameCenter/app/src/main/java/a207project/fall18/GameCenter/SlidingTilesBoardManager.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 import a207project.fall18.GameCenter.bean.Score;
 
@@ -13,7 +14,7 @@ import a207project.fall18.GameCenter.bean.Score;
  */
 public class SlidingTilesBoardManager extends BoardManager implements Serializable {
 
-    private Score score;
+    private Score score = new Score();
 
     /**
      * The tiles being managed.
@@ -26,29 +27,208 @@ public class SlidingTilesBoardManager extends BoardManager implements Serializab
 
     public int can_undo_time ;
 
+
+    public SlidingTilesBoardManager(SlidingTilesBoard board) {this.board = board;}
+
     /**
-     * Manage a tiles that has been pre-populated.
+     * Manage a board that has been pre-populated.
      *
-     * @param board the tiles
+     * @param tiles the board in 2D List
      */
-    public SlidingTilesBoardManager(SlidingTilesBoard board) {
-        this.board = board;
+    public SlidingTilesBoardManager(List<SlidingTile> tiles) {
+//        Board.setNumRowsCols(4);
+////        List<Tile> tiles = new ArrayList<>();
+//        final int numTiles = Board.NUM_ROWS * Board.NUM_COLS;
+//        for (int tileNum = 0; tileNum != numTiles; tileNum++) {
+//            tiles.add(new Tile(tileNum));
+//        }
+        this.board = new SlidingTilesBoard(tiles);
+//        this.board = board;
     }
 
     /**
-     * Manage a new shuffled tiles.
+     * Manage a new shuffled board.
      */
     SlidingTilesBoardManager() {
-        List<SlidingTile> slidingTiles = new ArrayList<>();
+        List<SlidingTile> tiles = new ArrayList<>();
         final int numTiles = SlidingTilesBoard.NUM_ROWS * SlidingTilesBoard.NUM_COLS;
         for (int tileNum = 0; tileNum != numTiles; tileNum++) {
-            slidingTiles.add(new SlidingTile(tileNum));
+            tiles.add(new SlidingTile(tileNum));
         }
 
-        Collections.shuffle(slidingTiles);
-        this.board = new SlidingTilesBoard(slidingTiles);
+        //        Collections.shuffle(tiles);
+//        System.out.println("wtf");
+//        while (!(((Board.NUM_ROWS % 2 != 0) && (findNumInversion(tiles) % 2 == 0)) ||
+//                ((Board.NUM_ROWS % 2 == 0) && ((blankOnOddRowFromBottom(tiles)) ==
+//                        (findNumInversion(tiles) % 2 == 0))))) {
+//            System.out.println("wtf2");
+//            Collections.shuffle(tiles);
+//        }
+
+
+
+//        if (!(((Board.NUM_ROWS % 2 != 0) && (findNumInversion(tiles) % 2 == 0)) ||
+//                ((Board.NUM_ROWS % 2 == 0) && ((blankOnOddRowFromBottom(tiles)) ==
+//                        (findNumInversion(tiles) % 2 == 0))))) {
+//            Collections.shuffle(tiles);
+//        }
+
+        // IDK MAN..
+//        tilesList = tiles;
+        this.board = new SlidingTilesBoard(tiles);
         score = new Score(MyApplication.getInstance().getUser(), "SlidingTiles");
+        makeBoard(tiles);
     }
+
+//    public List<Tile> getTilesList() {return tilesList;}
+
+    private void makeBoard(List<SlidingTile> lst) {
+        int blankIndex = findBlank(lst);
+
+        System.out.println(blankIndex);
+
+        Random r = new Random();
+        int bound = r.nextInt((500 - 450) + 1) + 450;
+
+        for (int i = 0; i < bound; i++) {
+
+            System.out.println("wtf" + i);
+
+            int row = blankIndex / SlidingTilesBoard.NUM_ROWS;
+            int col = blankIndex % SlidingTilesBoard.NUM_COLS;
+            String swapWith = pickRandomTile(row, col);
+            switch (swapWith) {
+                case "top":
+                    board.swapTiles(row, col, row - 1, col);
+                    if (SlidingTilesBoard.NUM_ROWS == 3) {blankIndex -= 3;}
+                    else if (SlidingTilesBoard.NUM_ROWS == 4) {blankIndex -= 4;}
+                    else {blankIndex -= 5;}
+                    break;
+                case "below":
+                    board.swapTiles(row, col, row + 1, col);
+                    if (SlidingTilesBoard.NUM_ROWS == 3) {blankIndex += 3;}
+                    else if (SlidingTilesBoard.NUM_ROWS == 4) {blankIndex += 4;}
+                    else {blankIndex += 5;}
+                    break;
+                case "left":
+                    board.swapTiles(row, col, row, col - 1);
+                    blankIndex -= 1;
+                    break;
+                case "right":
+                    board.swapTiles(row, col, row, col + 1);
+                    blankIndex += 1;
+                    break;
+            }
+        }
+
+    }
+
+
+//        Random r = new Random();
+//
+//        return r.nextInt((max - min) + 1) + min;
+
+    //max = (a > b) ? a : b;
+//        Tile above = row == 0 ? null : board.getTile(row - 1, col);
+//        Tile below = row == Board.NUM_ROWS - 1 ? null : board.getTile(row + 1, col);
+//        Tile left = col == 0 ? null : board.getTile(row, col - 1);
+//        Tile right = col == Board.NUM_COLS - 1 ? null : board.getTile(row, col + 1);
+
+//        if (Board.NUM_ROWS == 3) {
+//            int top = row == 0 ? null : blankIndex - 3;
+//            int below = row == Board.NUM_ROWS - 1 ? null :
+//        } else if (Board.NUM_ROWS == 4) {
+//
+//        } else {
+//
+//        }
+
+
+
+    private String pickRandomTile(int row, int col) {
+        List<String> listOfTiles = new ArrayList<>();
+        if (row != 0) {
+            listOfTiles.add("top");
+        } if (row != SlidingTilesBoard.NUM_ROWS - 1) {
+            listOfTiles.add("below");
+        } if (col != 0) {
+            listOfTiles.add("left");
+        } if (col != SlidingTilesBoard.NUM_COLS - 1) {
+            listOfTiles.add("right");
+        }
+        Collections.shuffle(listOfTiles);
+        return listOfTiles.get(0);
+    }
+
+
+
+//    int findNumInversion(List<Tile> lst) {
+//
+//        int numInversion = 0;
+//
+////        for (Tile tile : lst) {
+////            int currentTileNum = tile.getId();
+////            for (int i = lst.indexOf(tile) + 1; i < lst.size(); i++) {
+////                if (lst.get(i).getId() < currentTileNum) {numInversion += 1;}
+////            }
+////        }
+//
+//        for (int i = 0; i < lst.size(); i++) {
+//            int currentTileNum = lst.get(i).getId();
+//            if (currentTileNum != Board.NUM_ROWS * Board.NUM_COLS) {
+//                for (int x = i + 1; x < lst.size(); x++) {
+//                    if (lst.get(x).getId() < currentTileNum) {numInversion += 1;}
+//                }
+//            }
+//        }
+//
+//        return numInversion;
+//    }
+
+//    public static void main(String[] args) {
+//        List<Tile> tiles = new ArrayList<>();
+//        final int numTiles = 3 * 3;
+//        for (int tileNum = 0; tileNum != numTiles; tileNum++) {
+//            tiles.add(new Tile(tileNum));
+//        }
+//
+//        for (int i = 9; i < 10; i++) {
+//            System.out.println(99);
+//        }
+//
+//        Collections.shuffle(tiles);
+//        for (Tile t : tiles) {
+//            System.out.println(t.getId());
+//        }
+//
+//        System.out.println(findNumInversion(tiles));
+//
+//    }
+
+//    boolean blankOnOddRowFromBottom(List<Tile> lst) {
+//        for (int i = 0; i < lst.size(); i++) {
+//            if (lst.get(i).getId() == Board.NUM_ROWS * Board.NUM_COLS) {
+////                if (Board.NUM_ROWS == 3) {
+////                    if ((i <= 2) || (6 <= i)) {return true;}
+////                } else if (Board.NUM_ROWS == 4) {
+////                    if (((4 <= i) && (i <= 7)) || (i >= 12)) {return true;}
+////                } else {
+////                    if ((i <= 4) || ((10 <= i) && (i <= 14)) || (i >= 24)) {return true;}
+////                }
+//                if (((4 <= i) && (i <= 7)) || (i >= 12)) {return true;}
+//            }
+//        }
+//        return false;
+//    }
+
+    private int findBlank(List<SlidingTile> lst) {
+        int id = 0;
+        for (int i = 0; i < lst.size(); i++) {
+            if (lst.get(i).getId() == SlidingTilesBoard.NUM_ROWS * SlidingTilesBoard.NUM_COLS) {id = i;}
+        }
+        return id;
+    }
+
 
 
     /**
@@ -125,7 +305,7 @@ public class SlidingTilesBoardManager extends BoardManager implements Serializab
      *
      * @param position the position
      */
-    void touchMove(int position) {
+    public void touchMove(int position) {
         int row = position / SlidingTilesBoard.NUM_ROWS;
         int col = position % SlidingTilesBoard.NUM_COLS;
         int blankId = board.numTiles();
